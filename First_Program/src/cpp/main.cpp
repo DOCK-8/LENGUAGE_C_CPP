66adc2dd9189a2f2c64301d06a82fa12092e08f2
DOCK <franwillaa@gmail.com>
2024-10-26 11:50:58 -0400

    My first Hello World}

---
+++ First_Program/src/cpp/main.cpp
@@ -0,0 +1,25 @@
+/*
+*Include is a word for to import files or document,
+*important for the compile the program
+---------------------------------------------------
+
+<IOSTREAM>
+
+the iostream is a file in to compile, that it have
+a any function for to output and input fro console
+
+int main ()
+
+the principal funcion (main) is output a message,
+how the next example:
+
+Hello World
+
+*/
+
+#include <iostream>
+
+int main(int narg, char **arg){
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
